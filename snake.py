4ec86707c3c52c351e77be999917231ba8d4cf6e
Shubham Ambre <shubhamambre94@gmail.com>
2020-04-10 12:15:20 +0530

    made window and player

--- snake.py
+++ snake.py
@@ -1,2 +1,23 @@
 import pygame
+pygame.init()
+(width, height) = (500,500)
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Snake Game')
 
+x = 250
+y = 250
+width = 40
+height = 40
+vel = 5
+# clock = pygame.time.Clock()
+
+run = True
+while run:
+    pygame.time.delay(100)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False   
+    
+    pygame.draw.rect(screen, (255,0,0), (x,y, width, height))
+    pygame.display.update()
+pygame.quit()
