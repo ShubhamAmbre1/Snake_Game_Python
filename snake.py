ed4e0bfc0ef4145a4ceb2dd7d848244847c7f331
Shubham Ambre <shubhamambre94@gmail.com>
2020-04-12 18:59:47 +0530

    border collision

--- snake.py
+++ snake.py
@@ -5,13 +5,11 @@ pygame.init()
 screen = pygame.display.set_mode((500, 500))
 pygame.display.set_caption('Snake Game')
 
-x = 250                                     #position x
-y = 250                                     #position y
+x = 250                                     #snake's position on x
+y = 250                                     #snake's position on y
 
 width = 20                                  #snake width
 height = 20                                 #snake height
-
-vel = 5                                     #velocity
 
 x1_change = 0                               #change in position
 y1_change = 0                               #change in position
@@ -38,7 +36,8 @@ while run:
             elif event.key == pygame.K_DOWN and y1_change != -10:
                 x1_change = 0
                 y1_change = 10
-
+        if x >= 500 or x <= 0 or y >= 500 or y <= 0:
+            run = False
 
     x += x1_change
     y += y1_change
